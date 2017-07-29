51c89923ac85e856513197f7c0b4f1ba7b13ef4f
littlebassistjm <krishanhilario@gmail.com>
2017-07-29 17:11:55 +0800

    Station Fix
    Illegal State Monitor Exception fix
    -unsure, will monitor monitor changes

--- src/Station.java
+++ src/Station.java
@@ -9,35 +9,50 @@ public class Station {
     boolean station_occupied;
     Train current_train;
     int station_id;
-    int waiting_passengers = 1;
+    int waiting_passengers = 0;
     ArrayList<Passenger> list_waiting_passengers;
+    ArrayList<Train> list_waiting_trains;
 
     Station(int station_id) {
         this.station_id = station_id;
         station_occupied = false;
         current_train = null;
         list_waiting_passengers = new ArrayList<>();
+        list_waiting_trains = new ArrayList<>();
+
     }
 
     public void load_train(Train train) throws InterruptedException {
 
-        synchronized (train) {
+        synchronized (this) {
+            // check if a train is in the station already
             if (station_occupied) {
                 System.out.println("Station " + station_id + " is occupied. Train " + train.TRAIN_ID + " is waiting");
+                list_waiting_trains.add(train);
                 wait();
                 System.out.println("Station " + station_id + " is no longer occupied. Train " + train.TRAIN_ID + " is now entering.");
             }
 
-            station_occupied = true;
-            current_train = train;
-            System.out.println("Train " + train.TRAIN_ID + " has occupied Station " + station_id + ".");
-            if (waiting_passengers>0) {
-                on_board();
-            }
-            synchronized (current_train) {
-                current_train.wait();
-            }
         }
+
+        // occupy station
+        station_occupied = true;
+        current_train = train;
+        System.out.println("Train " + train.TRAIN_ID + " has occupied Station " + station_id + ".");
+
+        //start boarding
+        synchronized (current_train) {
+            if (waiting_passengers > 0) {
+                start_boarding();
+            }
+            current_train.wait(); // wait while passengers are boarding
+        }
+
+        //notify next train that the station is not available
+        synchronized (this) {
+            notify();
+        }
+        System.out.println("TEST");
     }
 
     public void wait_for_train(Passenger passenger) throws InterruptedException {
@@ -46,29 +61,37 @@ public class Station {
             list_waiting_passengers.add(passenger);
             System.out.println("Passenger " + waiting_passengers + " is now waiting for the train is Station " + station_id + ".");
             if (!station_occupied) wait();
+            else wait();
         }
+        System.out.println("Passenger " + waiting_passengers + " has been called to board");
         on_board();
     }
 
-    private void start_boarding(){
-        list_waiting_passengers.get(0).notify();
+    private void start_boarding() {
+        System.out.println("Train " + current_train.TRAIN_ID + " has started boarding.");
+        synchronized (this) {
+            notify();
+        }
     }
 
     public void on_board() {
+        System.out.println("Passenger " + waiting_passengers + " is boarding.");
         synchronized (this) {
             waiting_passengers--;
             //list_waiting_passengers.remove(0);
+            System.out.println("Remaining passengers: " + waiting_passengers);
         }
-      //  if (waiting_passengers == 0) {
-        synchronized (current_train) {
-            current_train.notify();
+        if (waiting_passengers == 0) {
+            synchronized (current_train) {
+                current_train.notify();
+            }
+            System.out.println("Train " + current_train.TRAIN_ID + " has left Station " + station_id);
+        } else {
+            synchronized (list_waiting_passengers.get(0)) {
+                list_waiting_passengers.get(0).notify();
+            }
         }
-        System.out.println("Train " + current_train.TRAIN_ID + " has left Station " + station_id);
-     //   }
-        //else
-        synchronized (list_waiting_passengers.get(0)) {
-            list_waiting_passengers.get(0).notify();
-        }
+
 
     }
 
